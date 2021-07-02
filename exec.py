f1be8dd542d663e8d9f7f1a2248fa1a81acff306
차한음 <ihaneum@chahan-eum-ui-MacBookAir.local>
2021-07-02 15:27:16 +0900

    v2.0

--- exec.py
+++ exec.py
@@ -2,8 +2,12 @@
 from datetime import *
 _today_ = datetime.now()
 print("-- 날짜 구하기 --")
-print("-> 형식: yyyymmdd <-")
+print("-> 형식: yyyy-mm-dd <-")
 any_date = str(input("날짜: "))
-_date_ = datetime.strptime(any_date, "%Y%m%d")
+_date = ''
+_date += any_date[:4]
+_date += any_date[5:7]
+_date += any_date[8:10]
+_date_ = datetime.strptime(_date, "%Y%m%d")
 diff = _date_ - _today_
-print("-- {}년 {}월 {}일 까지 {}일 남았어요 --".format(any_date[:4], any_date[4:6], any_date[6:8], diff.days))
+print("-> {}년 {}월 {}일 까지 {}일 남았어요 <-".format(_date[:4], _date[4:6], _date[6:8], diff.days))
